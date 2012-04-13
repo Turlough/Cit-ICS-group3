b8e1fd295c316cc1c5c449ca32d36df0941522ad
Turlough Cowman <turloughcowman@gmail.com>
2012-04-13 20:11:42 +0100

    Overhaul of Find Customers. Includes a display screen to right, like Find Properties. Uses new SQL Server view 'ActiveCustomers' (SQL creation code on website) Uses new SQL Server stored procedure, also on website Needs to be cleaned up- disable text boxes, show save button etc.

--- ICS Assignment/frmFindCustomer.Designer.vb
+++ ICS Assignment/frmFindCustomer.Designer.vb
@@ -22,100 +22,115 @@ Partial Class frmFindCustomer
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.Button3 = New System.Windows.Forms.Button()
+        Me.btnFind = New System.Windows.Forms.Button()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.cbxField = New System.Windows.Forms.ComboBox()
+        Me.dgvCustomers = New System.Windows.Forms.DataGridView()
         Me.Panel1 = New System.Windows.Forms.Panel()
         Me.btnArchive = New System.Windows.Forms.Button()
         Me.btnEdit = New System.Windows.Forms.Button()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.btnProperties = New System.Windows.Forms.Button()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtPhone = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtAddress = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtSname = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtFname = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.dgvCustomers, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TextBox3
+        'txtSearch
         '
-        Me.TextBox3.Location = New System.Drawing.Point(39, 7)
-        Me.TextBox3.Margin = New System.Windows.Forms.Padding(2)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(76, 20)
-        Me.TextBox3.TabIndex = 6
+        Me.txtSearch.Location = New System.Drawing.Point(38, 12)
+        Me.txtSearch.Margin = New System.Windows.Forms.Padding(2)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(196, 20)
+        Me.txtSearch.TabIndex = 6
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(8, 10)
+        Me.Label3.Location = New System.Drawing.Point(7, 14)
         Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(27, 13)
         Me.Label3.TabIndex = 7
         Me.Label3.Text = "Find"
         '
-        'Button3
+        'btnFind
         '
-        Me.Button3.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Button3.Location = New System.Drawing.Point(378, 6)
-        Me.Button3.Margin = New System.Windows.Forms.Padding(2)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(56, 19)
-        Me.Button3.TabIndex = 8
-        Me.Button3.Text = "Find"
-        Me.Button3.UseVisualStyleBackColor = True
+        Me.btnFind.Location = New System.Drawing.Point(378, 13)
+        Me.btnFind.Margin = New System.Windows.Forms.Padding(2)
+        Me.btnFind.Name = "btnFind"
+        Me.btnFind.Size = New System.Drawing.Size(56, 19)
+        Me.btnFind.TabIndex = 8
+        Me.btnFind.Text = "Find"
+        Me.btnFind.UseVisualStyleBackColor = True
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(130, 7)
+        Me.Label4.Location = New System.Drawing.Point(246, 16)
         Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(21, 13)
         Me.Label4.TabIndex = 9
         Me.Label4.Text = "by:"
         '
-        'ComboBox1
+        'cbxField
         '
-        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"First Name", "Second Name", "Email", "Phone", "Address"})
-        Me.ComboBox1.Location = New System.Drawing.Point(154, 7)
-        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(2)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(92, 21)
-        Me.ComboBox1.TabIndex = 10
+        Me.cbxField.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbxField.FormattingEnabled = True
+        Me.cbxField.Items.AddRange(New Object() {"fname", "lname", "Email", "Phone", "Address"})
+        Me.cbxField.Location = New System.Drawing.Point(282, 11)
+        Me.cbxField.Margin = New System.Windows.Forms.Padding(2)
+        Me.cbxField.Name = "cbxField"
+        Me.cbxField.Size = New System.Drawing.Size(92, 21)
+        Me.cbxField.TabIndex = 10
         '
-        'DataGridView1
+        'dgvCustomers
         '
-        Me.DataGridView1.AllowUserToAddRows = False
-        Me.DataGridView1.AllowUserToDeleteRows = False
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
-        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.ReadOnly = True
-        Me.DataGridView1.RowHeadersVisible = False
-        Me.DataGridView1.RowTemplate.Height = 24
-        Me.DataGridView1.Size = New System.Drawing.Size(433, 277)
-        Me.DataGridView1.TabIndex = 11
+        Me.dgvCustomers.AllowUserToAddRows = False
+        Me.dgvCustomers.AllowUserToDeleteRows = False
+        Me.dgvCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvCustomers.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvCustomers.Location = New System.Drawing.Point(0, 0)
+        Me.dgvCustomers.Margin = New System.Windows.Forms.Padding(2)
+        Me.dgvCustomers.Name = "dgvCustomers"
+        Me.dgvCustomers.ReadOnly = True
+        Me.dgvCustomers.RowHeadersVisible = False
+        Me.dgvCustomers.RowTemplate.Height = 24
+        Me.dgvCustomers.Size = New System.Drawing.Size(448, 418)
+        Me.dgvCustomers.TabIndex = 11
         '
         'Panel1
         '
         Me.Panel1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Panel1.Controls.Add(Me.DataGridView1)
-        Me.Panel1.Location = New System.Drawing.Point(1, 32)
+        Me.Panel1.Controls.Add(Me.dgvCustomers)
+        Me.Panel1.Location = New System.Drawing.Point(0, 37)
         Me.Panel1.Margin = New System.Windows.Forms.Padding(2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(433, 277)
+        Me.Panel1.Size = New System.Drawing.Size(448, 418)
         Me.Panel1.TabIndex = 12
         '
         'btnArchive
         '
         Me.btnArchive.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btnArchive.Location = New System.Drawing.Point(369, 314)
+        Me.btnArchive.Location = New System.Drawing.Point(183, 436)
         Me.btnArchive.Margin = New System.Windows.Forms.Padding(2)
         Me.btnArchive.Name = "btnArchive"
         Me.btnArchive.Size = New System.Drawing.Size(56, 19)
@@ -126,7 +141,7 @@ Partial Class frmFindCustomer
         'btnEdit
         '
         Me.btnEdit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.btnEdit.Location = New System.Drawing.Point(300, 314)
+        Me.btnEdit.Location = New System.Drawing.Point(123, 436)
         Me.btnEdit.Margin = New System.Windows.Forms.Padding(2)
         Me.btnEdit.Name = "btnEdit"
         Me.btnEdit.Size = New System.Drawing.Size(56, 19)
@@ -134,35 +149,172 @@ Partial Class frmFindCustomer
         Me.btnEdit.Text = "Edit"
         Me.btnEdit.UseVisualStyleBackColor = True
         '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.SplitContainer1.Location = New System.Drawing.Point(7, 12)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label3)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.txtSearch)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.btnFind)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label4)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Panel1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.cbxField)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnProperties)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtEmail)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label7)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtPhone)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label6)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtAddress)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label5)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtSname)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label2)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.txtFname)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Label1)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnEdit)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.btnArchive)
+        Me.SplitContainer1.Size = New System.Drawing.Size(695, 457)
+        Me.SplitContainer1.SplitterDistance = 450
+        Me.SplitContainer1.TabIndex = 14
+        '
+        'btnProperties
+        '
+        Me.btnProperties.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnProperties.Location = New System.Drawing.Point(43, 436)
+        Me.btnProperties.Name = "btnProperties"
+        Me.btnProperties.Size = New System.Drawing.Size(75, 19)
+        Me.btnProperties.TabIndex = 24
+        Me.btnProperties.Text = "Properties"
+        Me.btnProperties.UseVisualStyleBackColor = True
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(78, 126)
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(148, 20)
+        Me.txtEmail.TabIndex = 23
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(30, 129)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(32, 13)
+        Me.Label7.TabIndex = 22
+        Me.Label7.Text = "Email"
+        '
+        'txtPhone
+        '
+        Me.txtPhone.Location = New System.Drawing.Point(78, 91)
+        Me.txtPhone.Name = "txtPhone"
+        Me.txtPhone.Size = New System.Drawing.Size(148, 20)
+        Me.txtPhone.TabIndex = 21
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(24, 94)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(38, 13)
+        Me.Label6.TabIndex = 20
+        Me.Label6.Text = "Phone"
+        '
+        'txtAddress
+        '
+        Me.txtAddress.Location = New System.Drawing.Point(78, 152)
+        Me.txtAddress.Multiline = True
+        Me.txtAddress.Name = "txtAddress"
+        Me.txtAddress.Size = New System.Drawing.Size(148, 101)
+        Me.txtAddress.TabIndex = 19
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(17, 155)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(45, 13)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "Address"
+        '
+        'txtSname
+        '
+        Me.txtSname.Location = New System.Drawing.Point(78, 60)
+        Me.txtSname.Name = "txtSname"
+        Me.txtSname.Size = New System.Drawing.Size(148, 20)
+        Me.txtSname.TabIndex = 17
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(18, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 13)
+        Me.Label2.TabIndex = 16
+        Me.Label2.Text = "Surname"
+        '
+        'txtFname
+        '
+        Me.txtFname.Location = New System.Drawing.Point(78, 27)
+        Me.txtFname.Name = "txtFname"
+        Me.txtFname.Size = New System.Drawing.Size(148, 20)
+        Me.txtFname.TabIndex = 15
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(8, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 13)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Forename"
+        '
         'frmFindCustomer
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(434, 338)
-        Me.Controls.Add(Me.btnEdit)
-        Me.Controls.Add(Me.btnArchive)
-        Me.Controls.Add(Me.Panel1)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.TextBox3)
+        Me.ClientSize = New System.Drawing.Size(714, 472)
+        Me.Controls.Add(Me.SplitContainer1)
         Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "frmFindCustomer"
-        Me.Text = "Form2"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Text = "Find Customer"
+        CType(Me.dgvCustomers, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
     Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents btnFind As System.Windows.Forms.Button
     Friend WithEvents Label4 As System.Windows.Forms.Label
-    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
-    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents cbxField As System.Windows.Forms.ComboBox
+    Friend WithEvents dgvCustomers As System.Windows.Forms.DataGridView
     Friend WithEvents Panel1 As System.Windows.Forms.Panel
     Friend WithEvents btnArchive As System.Windows.Forms.Button
     Friend WithEvents btnEdit As System.Windows.Forms.Button
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+    Friend WithEvents btnProperties As System.Windows.Forms.Button
+    Friend WithEvents txtEmail As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents txtPhone As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtAddress As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtSname As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtFname As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
 End Class
