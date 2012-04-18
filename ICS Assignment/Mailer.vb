81d2f1ad056c00f3450c4a0ca272abe667ca01c7
Turlough Cowman <turloughcowman@gmail.com>
2012-04-18 13:37:10 +0100

    Added the new files to commit- forgot first time

---
+++ ICS Assignment/Mailer.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Net.Mail
+Public Class Mailer
+    Dim cust As New Customer
+    Dim prop As New Properties
+    Dim server As String = ""
+
+
+    Public Sub MakeOffer(OwnerMail As String, BuyerMail As String, prop As Properties)
+        'TODO Mailer.MakeOffer
+    End Sub
+    Sub notifyNewProperty()
+        'TODO Mailer.notifyNewProperty
+    End Sub
+End Class
