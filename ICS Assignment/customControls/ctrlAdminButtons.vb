455ebe59807ec9dd2ff811e810d0ce970e245416
Turlough Cowman <turloughcowman@gmail.com>
2012-04-25 14:11:46 +0100

    added events to ctrlAdminButtons

--- ICS Assignment/customControls/ctrlAdminButtons.vb
+++ ICS Assignment/customControls/ctrlAdminButtons.vb
@@ -3,6 +3,10 @@
     Public dSql As String
     Public eSql As String
 
+    Public Event Saved As EventHandler
+    Public Event EnableEdit As EventHandler
+    Public Event Deleted As EventHandler
+
     Private Sub btnDelete_Click(sender As System.Object, e As System.EventArgs) Handles btnDelete.Click
         'confirm
         If MsgBox("Confirm Delete", MsgBoxStyle.OkCancel) = MsgBoxResult.Cancel Then Exit Sub
@@ -10,6 +14,8 @@
         Dim dbc As New DBConnector
         dbc.execute(dSql)
         dbc = Nothing
+
+        RaiseEvent Deleted(Me, New EventArgs)
     End Sub
 
 
@@ -17,11 +23,15 @@
         If btnEdit.Text = "Edit" Then
 
             btnEdit.Text = "Save"
+
+            RaiseEvent EnableEdit(Me, New EventArgs)
         Else
             Dim dbc As New DBConnector
             dbc.execute(eSql)
             btnEdit.Text = "Edit"
             dbc = Nothing
+
+            RaiseEvent Saved(Me, New EventArgs)
         End If
     End Sub
 End Class
