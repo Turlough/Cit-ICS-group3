3aa8fd2aea23bd7241242c56cdcfc8d8079ae910
Turlough Cowman <turloughcowman@gmail.com>
2012-04-25 11:47:20 +0100

    Created user control ctrlAdminButtons (edit and delete buttons)

---
+++ ICS Assignment/customControls/ctrlAdminButtons.vb
@@ -0,0 +1,20 @@
+﻿Public Class ctrlAdminButtons
+
+    Private Sub btnDelete_Click(sender As System.Object, e As System.EventArgs) Handles btnDelete.Click
+        'confirm
+        If MsgBox("Confirm Delete", MsgBoxStyle.OkCancel) = MsgBoxResult.Cancel Then Exit Sub
+        'delete
+
+    End Sub
+
+
+    Private Sub btnEdit_Click(sender As System.Object, e As System.EventArgs) Handles btnEdit.Click
+        If btnDelete.Text = "Edit" Then
+
+            btnDelete.Text = "Save"
+        Else
+
+            btnDelete.Text = "Edit"
+        End If)
+    End Sub
+End Class
